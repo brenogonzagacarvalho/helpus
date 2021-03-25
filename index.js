3c7fb5fa4e14673f57e787acc1280cad89e74f1b
Breno Gonzaga <brenogonzaga96@gmail.com>
2021-03-25 19:54:08 -0300

    edicao das pastas

--- index.js
+++ index.js
@@ -11,14 +11,18 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
-app.set("app", path.resolve(__dirname, "views/ejs"));
-app.use('/css', express.static(path.resolve(__dirname,"front-end/css")));
-app.use('/js', express.static(path.resolve(__dirname,"front-end/js")));
-app.use('/img', express.static(path.resolve(__dirname,"front-end/img")));
+//app.set("app", path.resolve(__dirname, "views/ejs"));
+
+app.use('/css', express.static(path.resolve(__dirname,"assets/css")));
+app.use('/js', express.static(path.resolve(__dirname,"assets/js")));
+app.use('/img', express.static(path.resolve(__dirname,"assets/img")));
 
 app.get('/login',(req,res) =>{
-    res.send();
-})
+    res.render('login');
+});
+app.get('/cadastro',(req,res) =>{
+    res.render('cadastro');
+});
 
 //require('./app/controllers/index.js')(app);
 app.listen(PORT, ()=> { console.log(`Server is running on http://localhost:${PORT}`)});
